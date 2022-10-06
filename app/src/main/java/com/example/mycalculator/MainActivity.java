397a800b29aa8cb4bc3a243fd872c992fc8e046f
Noah Sprenger <noahsprengersonline@gmail.com>
2022-10-06 17:06:45 -0400

    add division

--- app/src/main/java/com/example/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -110,7 +110,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btn_multiply:
                 addNumber("x");
                 break;
-            case R.id.btn_equal:
+	    case R.id.btn_divide:
+		addNumber("/");
+		break;
+	    case R.id.btn_equal:
                 String result = null;
                 try {
                     result = evaluate(text_display.getText().toString());
